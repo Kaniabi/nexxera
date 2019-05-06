34d6dd2d491927ba18116d467eeb185825206400
Alexandre Andrade <kaniabi@gmail.com>
2019-05-06 13:55:10 +0000

    FEA: Soft deletion, total amount and Max Amount
    * FEA: Implemented a soft-deletion mechanism for transactions;
    * FEA: Added total_amount to GET transactions;
    * FEA: Setting status=ERRO if the transaction amount is greater than
    100000.00;
    * IMP: Making all transaction fields required;
    * IMP: Added tests for filtered GET transactions.

--- nix/nexxera/nix/app.py
+++ nix/nexxera/nix/app.py
@@ -2,7 +2,7 @@ def create_app(name, config_updates=None):
     import flask
     import flask_sqlalchemy
     import flask_restless
-    from .models import create_models
+    from .models import create_models, ValidationError
 
     result = flask.Flask(name)
     result.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///nix.db"
@@ -14,7 +14,18 @@ def create_app(name, config_updates=None):
     result.db.create_all()
 
     manager = flask_restless.APIManager(result, flask_sqlalchemy_db=result.db)
-    manager.create_api(result.models.NixUser, methods=["GET", "POST", "DELETE"])
-    manager.create_api(result.models.NixTransaction, methods=["GET", "POST", "DELETE"])
+    manager.create_api(
+        result.models.NixUser,
+        methods=["GET", "POST", "DELETE"],
+        validation_exceptions=[ValidationError]
+    )
+    manager.create_api(
+        result.models.NixTransaction,
+        methods=["GET", "POST", "DELETE"],
+        validation_exceptions=[ValidationError],
+        postprocessors={
+            'GET_MANY': [result.models.NixTransaction.get_many_postprocessor]
+        }
+    )
 
     return result
--- nix/nexxera/nix/models.py
+++ nix/nexxera/nix/models.py
@@ -1,10 +1,58 @@
-def create_models(db):
-    import enum
+from flask_sqlalchemy import BaseQuery
 
-    class TransactionType(enum.Enum):
-        CC = "CC"
-        TED = "TED"
-        DOC = "DOC"
+
+class ValidationError(BaseException):
+
+    def __init__(self, **errors):
+        super(ValidationError, self).__init__()
+        self.errors = errors
+
+
+def create_models(db):
+    from sqlalchemy.orm import validates
+
+    # import enum
+    # class TransactionType(enum.Enum):
+    #     CC = "CC"
+    #     TED = "TED"
+    #     DOC = "DOC"
+
+    class QueryWithSoftDelete(BaseQuery):
+        """
+        This class provides an alternative query implementation with support for soft-deletion.
+
+        Reference: https://blog.miguelgrinberg.com/post/implementing-the-soft-delete-pattern-with-flask-and-sqlalchemy
+        """
+
+        _with_deleted = False
+
+        def __new__(cls, *args, **kwargs):
+            obj = super(QueryWithSoftDelete, cls).__new__(cls)
+            obj._with_deleted = kwargs.pop('_with_deleted', False)
+            if len(args) > 0:
+                super(QueryWithSoftDelete, obj).__init__(*args, **kwargs)
+                return obj.filter_by(deleted=False) if not obj._with_deleted else obj
+            return obj
+
+        def __init__(self, *args, **kwargs):
+            pass
+
+        def with_deleted(self):
+            return self.__class__(
+                db.class_mapper(self._mapper_zero().class_),
+                session=db.session(),
+                _with_deleted=True
+            )
+
+        def _get(self, *args, **kwargs):
+            # this calls the original query.get function from the base class
+            return super(QueryWithSoftDelete, self).get(*args, **kwargs)
+
+        def get(self, *args, **kwargs):
+            # the query.get method does not like it if there is a filter clause
+            # pre-loaded, so we need to implement it using a workaround
+            obj = self.with_deleted()._get(*args, **kwargs)
+            return obj if obj is None or self._with_deleted or not obj.deleted else None
 
     class User(db.Model):
         __tablename__ = "users"
@@ -16,26 +64,55 @@ def create_models(db):
     class Transaction(db.Model):
         __tablename__ = "transactions"
 
+        # The maximum amount accepted by a transaction.
+        MAX_AMOUNT = 100000.00
+
         id = db.Column(db.Integer, primary_key=True)
         user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
         # user = db.relationship("User", backref=db.backref("transactions", lazy=True))
 
-        # # Creditor Account
-        # creditor_name = db.Column(db.String(128))
-        # creditor_bank = db.Column(db.String(3))
-        # creditor_agency = db.Column(db.String(4))
-        # creditor_account = db.Column(db.String(6))
-        #
-        # # Debtor Account
-        # debtor_name = db.Column(db.String(128))
-        # debtor_bank = db.Column(db.String(3))
-        # debtor_agency = db.Column(db.String(4))
-        # debtor_account = db.Column(db.String(6))
+        # Creditor Account
+        creditor_name = db.Column(db.String(128), nullable=False)
+        creditor_bank = db.Column(db.String(3), nullable=False)
+        creditor_agency = db.Column(db.String(4), nullable=False)
+        creditor_account = db.Column(db.String(6), nullable=False)
+
+        # Debtor Account
+        debtor_name = db.Column(db.String(128), nullable=False)
+        debtor_bank = db.Column(db.String(3), nullable=False)
+        debtor_agency = db.Column(db.String(4), nullable=False)
+        debtor_account = db.Column(db.String(6), nullable=False)
 
         amount = db.Column(db.Float)
 
         # TODO: Should be an enumerate of TransactionType
         type = db.Column(db.String(3))
+
+        status = db.Column(db.String(12), default='OK')
+
+        # Soft deletion
+        query_class = QueryWithSoftDelete
+        deleted = db.Column(db.Boolean, default=False)
+
+        @validates('amount')
+        def validate_amount(self, _key, value):
+            """
+            The amount must be lower than MAX_AMOUNT.
+            """
+            if value > self.MAX_AMOUNT:
+                self.status = 'ERRO'
+            return value
+
+        @classmethod
+        def get_many_postprocessor(cls, result=None, search_params=None, **kw):
+            """
+            Adds total_amount to the GET MANY results.
+            """
+            total_amount = sum(
+                [i['amount'] for i in result['objects']]
+            )
+            result['total_amount'] = total_amount
+            return result
 
     class Models:
         NixUser = User
